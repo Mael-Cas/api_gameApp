210c0e6b684658c518afa422c1bca13611edbebb
remidesjardins <remi.desjardins@efrei.net>
2025-04-08 14:38:28 +0200

    App.js et db.js

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const express = require('express');
-const app = express();
 require('dotenv').config();
+const mysql = require('mysql2');
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
 
+db.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la BDD :', err.message);
+        process.exit(1); // Arrêter si la connexion échoue
+    }
+    console.log('✅ Connecté à la base de données MySQL');
+});
 
-app.use(express.json());
+// Route de test
+app.get('/', (req, res) => {
+    res.send('Serveur Express + MySQL opérationnel');
+});
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Démarrage du serveur
+app.listen(PORT, () => {
+    console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
+});
--- db.js
+++ db.js
@@ -1,11 +1,14 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const con = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
-
-module.exports = pool;
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
